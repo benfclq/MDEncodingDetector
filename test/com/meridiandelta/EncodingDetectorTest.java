f7513fa606c0e0938576962529e1b8555201d76d
Ben Myers <benfclq@inbox.com>
2013-03-06 13:38:32 +0000

    Cleaned up Encoding test code so that location on hard disk will not affect locating the test html files

--- test/com/meridiandelta/EncodingDetectorTest.java
+++ test/com/meridiandelta/EncodingDetectorTest.java
@@ -5,6 +5,7 @@
 package com.meridiandelta;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import junit.framework.Assert;
@@ -20,46 +21,47 @@ import static org.junit.Assert.*;
  * @author Ben
  */
 public class EncodingDetectorTest {
-    
+
     public EncodingDetectorTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
-    public void test_encodingutf8_filePassedAsStringName_correctyDetected() throws FileNotFoundException {
-        // TODO review the generated test code and remove the default call to fail.
-       // Path p1 = Paths.get(".");
-        //System.out.println(p1.toAbsolutePath().toString());
-        //System.out.println("Hello World!");
-       // System.out.println("Working Directory = " +  System.getProperty("user.dir"));
-        String fileName = "C:\\Documents and Settings\\Ben\\My Documents\\NetBeansProjects\\MDEncodingDetector\\src\\TestDocuments\\UTF8.htm";
-        EncodingDetector myEnc = new EncodingDetector(fileName);
+    public void test_encodingutf8_filePassedAsStringName_correctyDetected() throws FileNotFoundException, IOException {
+        String fileName = "UTF8.htm";
+        EncodingDetector myEnc = new EncodingDetector(getTestDocFullStringPath(fileName));
         Assert.assertEquals("Incorrect Encoding detected", "UTF-8", myEnc.detect());
-       // Assert.
+        // Assert.
         //fail("The test case is a prototype.");
     }
-    
+
     @Test
-    public void test_encodingutf8_filePassedAsPathObject_correctyDetected() throws FileNotFoundException {
-        String fileName = "C:\\Documents and Settings\\Ben\\My Documents\\NetBeansProjects\\MDEncodingDetector\\src\\TestDocuments\\UTF8.htm";
-        Path filePath = Paths.get(fileName);
+    public void test_encodingutf8_filePassedAsPathObject_correctyDetected() throws FileNotFoundException, IOException {
+        String fileName = "UTF8.htm";
+        Path filePath = Paths.get(getTestDocFullStringPath(fileName));
         EncodingDetector myEnc = new EncodingDetector(filePath);
         Assert.assertEquals("Incorrect Encoding detected", "UTF-8", myEnc.detect());
     }
+
+    private String getTestDocFullStringPath(String fileName) throws IOException {
+        Path p1 = Paths.get(".");
+        Path p2 = p1.resolve("src\\TestDocuments\\" + fileName);
+        return p2.toRealPath().toString();
         
+    }
 }
